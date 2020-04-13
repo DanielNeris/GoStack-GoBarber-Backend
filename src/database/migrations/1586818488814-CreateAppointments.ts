fb8a4a7f504c4540a469f4a18dcb1db64b236efa
Daniel de Souza Neris <danielneris01@gmail.com>
2020-04-13 20:33:15 -0300

    Repositório do TypeORM

--- src/database/migrations/1586818488814-CreateAppointments.ts
+++ src/database/migrations/1586818488814-CreateAppointments.ts
@@ -12,6 +12,7 @@ export default class CreateAppointments1586818488814
             type: 'varchar',
             isPrimary: true,
             generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
           },
           {
             name: 'provider',
