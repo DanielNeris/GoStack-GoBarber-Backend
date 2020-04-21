3d96c6749713e8e6572c975f480cc596d8c594d5
Daniel de Souza Neris <danielneris01@gmail.com>
2020-04-21 15:03:13 -0300

    Criação de registros

---
+++ src/services/CreateUserService.ts
@@ -0,0 +1,33 @@
+import { getRepository } from 'typeorm';
+
+import User from '../models/User';
+
+interface Resquest {
+  name: string;
+  email: string;
+  password: string;
+}
+
+class CreateUserService {
+  public async execute({ name, email, password }: Resquest): Promise<User> {
+    const usersRepository = getRepository(User);
+
+    const checkUserExists = await usersRepository.findOne({ where: { email } });
+
+    if (checkUserExists) {
+      throw new Error('Email address alredy used');
+    }
+
+    const user = usersRepository.create({
+      name,
+      email,
+      password,
+    });
+
+    await usersRepository.save(user);
+
+    return user;
+  }
+}
+
+export default CreateUserService;
