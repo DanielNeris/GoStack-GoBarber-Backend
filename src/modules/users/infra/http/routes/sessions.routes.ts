567779b4b65413b26ef9995c3f7e9bfb97aac53d
Daniel de Souza Neris <danielneris01@gmail.com>
2020-05-07 14:08:00 -0300

    Usando controllers

--- src/modules/users/infra/http/routes/sessions.routes.ts
+++ src/modules/users/infra/http/routes/sessions.routes.ts
@@ -1,25 +1,10 @@
 import { Router } from 'express';
 
-import UsersRepository from '@modules/users/infra/typeorm/repositories/UsersRepository';
-import AuthenticateUserService from '@modules/users/services/AuthenticateUserService';
+import SessionController from '../controllers/SessionController';
 
 const sessionsRouter = Router();
+const sessionController = new SessionController();
 
-sessionsRouter.post('/', async (req, res) => {
-  const usersRepository = new UsersRepository();
-
-  const { email, password } = req.body;
-
-  const authenticateUser = new AuthenticateUserService(usersRepository);
-
-  const { user, token } = await authenticateUser.execute({
-    email,
-    password,
-  });
-
-  delete user.password;
-
-  return res.json({ user, token });
-});
+sessionsRouter.post('/', sessionController.create);
 
 export default sessionsRouter;
