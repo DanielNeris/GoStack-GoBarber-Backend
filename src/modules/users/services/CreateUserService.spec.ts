1371d1c8a2a0e9bce570179ef351b7c291d5ff13
Daniel de Souza Neris <danielneris01@gmail.com>
2020-05-08 15:18:59 -0300

    Testando criação de usuário

--- src/modules/users/services/CreateUserService.spec.ts
+++ src/modules/users/services/CreateUserService.spec.ts
@@ -8,11 +8,30 @@ describe('CreateUser', () => {
     const createUserService = new CreateUserService(fakeUsersRepository);
 
     const user = await createUserService.execute({
-      name: 'Daniel',
+      name: 'Daniel Neris',
       email: 'daniel@daniel.com',
       password: '123456',
     });
 
     expect(user).toHaveProperty('id');
+  });
+
+  it('should not be able to create a new user with same email from another', async () => {
+    const fakeUsersRepository = new FakeUsersRepository();
+    const createUserService = new CreateUserService(fakeUsersRepository);
+
+    await createUserService.execute({
+      name: 'Daniel Neris',
+      email: 'daniel@daniel.com',
+      password: '123456',
+    });
+
+    expect(
+      createUserService.execute({
+        name: 'Daniel Neris',
+        email: 'daniel@daniel.com',
+        password: '123456',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
   });
 });
