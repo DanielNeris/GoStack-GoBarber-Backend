a5056b489a4026e2e49f805139c76dc6908d82b3
Daniel de Souza Neris <danielneris01@gmail.com>
2020-05-14 17:50:33 -0300

    Reset de senha

--- src/modules/users/services/SendForgotPasswordEmailService.ts
+++ src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -1,7 +1,6 @@
 import { injectable, inject } from 'tsyringe';
 
 import AppError from '@shared/errors/AppError';
-// import User from '../infra/typeorm/entities/User';
 import IMaillProvider from '@shared/container/providers/MailProvider/models/IMaillProvider';
 import IUsersRepository from '../repositories/IUsersRepository';
 import IUserTokensRepository from '../repositories/IUserTokensRepository';
