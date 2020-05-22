156b0a70f5c082566ac93a7d8366ab9e74c52961
Daniel de Souza Neris <danielneris01@gmail.com>
2020-05-22 20:17:26 -0300

    Rotas e controllers

---
+++ src/modules/appointments/infra/http/controllers/ProviderDayAvailabilityController.ts
@@ -0,0 +1,25 @@
+import { Request, Response } from 'express';
+import { container } from 'tsyringe';
+
+import ListProviderDayAvailabilityService from '@modules/appointments/services/ListProviderDayAvailabilityService';
+
+export default class ProviderDayAvailabilityController {
+  public async index(req: Request, res: Response): Promise<Response> {
+    const { provider_id } = req.params;
+
+    const { month, year, day } = req.body;
+
+    const listProviderDayAvailabilityService = container.resolve(
+      ListProviderDayAvailabilityService,
+    );
+
+    const availability = await listProviderDayAvailabilityService.execute({
+      provider_id,
+      year,
+      month,
+      day,
+    });
+
+    return res.json(availability);
+  }
+}
