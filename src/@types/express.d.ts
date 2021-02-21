f3a3dea6bb8734170ee897c625993b85574519ff
Dainel de Souza Neris <danielneris01@gmail.com>
2021-02-20 22:54:43 -0300

    update CI and test pullrequest

--- src/@types/express.d.ts
+++ src/@types/express.d.ts
@@ -1,4 +1,6 @@
 declare namespace Express {
+  /* eslint-disable  @typescript-eslint/interface-name-prefix */
+  /* eslint-disable  @typescript-eslint/naming-convention */
   export interface Request {
     user: {
       id: string;
